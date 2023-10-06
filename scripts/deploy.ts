0bf029e7dc3c48cd81485a7465a5030ed801172d
IliaAzhel <aje-ilia2013@yandex.by>
2023-10-06 18:01:41 +0300

    fix deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,13 @@
 const hre = require('hardhat');
-import { TestERC20 } from '../typechain';
 
 async function main() {
-    const args = [];
 
-    const factory = await hre.ethers.getContractFactory('TestERC20');
-    const contract = (await factory.deploy(...args)) as TestERC20;
+    const factory = await hre.ethers.getContractFactory('V3Fees');
+    const contract = await factory.deploy([])
 
     await contract.deployed();
 
     console.log('Contract deployed to:', contract.address);
-
-    await new Promise(r => setTimeout(r, 30000));
-
-    await hre.run('verify:verify', {
-        address: contract.address,
-        constructorArguments: args
-    });
 }
 
 main()
